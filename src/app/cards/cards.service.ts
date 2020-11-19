1571013ac6757b58b70b476d7eb6e148f9ea089f
Adrian Cerdeira <acwebdesign01@gmail.com>
2020-11-19 21:06:11 +0100

    resolve json added and service improved

--- src/app/cards/cards.service.ts
+++ src/app/cards/cards.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import cards from './cards.json';
 
 @Injectable({
   providedIn: 'root',
@@ -7,32 +8,53 @@ export class CardsService {
   private cards;
 
   constructor() {
+    // Initialisierung des Karten Array
     this.cards = this.createAll();
   }
 
   getAll() {
-
+    return this.cards;
   }
 
-  getById() {
+  getById(id: number) {
+    return this.cards.find(c => c.id === id);
+  }
 
+  getByName(name: string) {
+    return this.cards.find(c => c.name === name);
   }
 
   private createAll() {
-    return [
-      this.create({
-      })
-    ];
+    // Erstellt Karten Array in alphabetisch nach Namen sortiert
+    return cards
+      .sort((a, b) => a.name.localeCompare(b.name))
+      .map((c, i) => {
+
+        return this.create({
+          id: i + 1,
+          name: c.name,
+          img: c.img,
+          description: c.description,
+          effects: c.effects,
+          attack: c.attack,
+          life: c.life,
+          shield: c.shield,
+          crystal: c.crystal,
+          type: c.type,
+        })
+
+      });
   }
 
-  private create(template) {
+  private create(template: any) {
     const newCard = {
       id: null,
       name: null,
+      img: null,
       description: null,
       effects: null,
       attack: null,
-      live: null,
+      life: null,
       shield: null,
       crystal: null,
       type: null,
