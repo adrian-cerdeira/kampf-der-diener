b4adbb571fe1de0c2545219a5c23ffa2f7a2afe8
adrian_cerdeira <acwebdesign01@gmail.com>
2020-11-26 11:09:12 +0100

    searchbox code integrated

--- src/app/shared/search-box/search-box.component.ts
+++ src/app/shared/search-box/search-box.component.ts
@@ -1,4 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+import { distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-search-box',
@@ -6,11 +9,22 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./search-box.component.scss']
 })
 export class SearchBoxComponent implements OnInit {
+  @Input() searchTerm: any;
   @Input() placeholder = 'Suchen ...';
+  @ViewChild('searchElement') searchElement: any;
+  @Output() searchChanged = new EventEmitter();
+  searchControl = new FormControl();
 
   constructor() { }
 
   ngOnInit(): void {
+    this.searchControl.setValue(this.searchTerm || '');
+    this.searchControl.valueChanges.pipe(
+      // wait 500ms after last change
+      // debounceTime(500), // nötig für online-abfragen, dann müsste man noch den switchMap (im HttpService) hinzufügen
+      // check if new value differs from old one
+      distinctUntilChanged(),
+    ).subscribe(term => this.searchChanged.emit(term));
   }
 
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { FooterComponent } from './footer/footer.component';
@@ -10,11 +11,15 @@ import { SearchBoxComponent } from './search-box/search-box.component';
   imports: [
     CommonModule,
     RouterModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   exports: [
     ToolbarComponent,
     FooterComponent,
-    SearchBoxComponent
+    SearchBoxComponent,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   declarations: [
     ToolbarComponent,
