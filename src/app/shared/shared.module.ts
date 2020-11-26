dd3e5286d152ba44c7285a49a91bcd4efacfd1e1
adrian_cerdeira <acwebdesign01@gmail.com>
2020-11-26 10:32:30 +0100

    Restliche Routen der Seite in Toolbar integriert

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { FooterComponent } from './footer/footer.component';
@@ -7,6 +8,7 @@ import { FooterComponent } from './footer/footer.component';
 @NgModule({
   imports: [
     CommonModule,
+    RouterModule,
   ],
   exports: [
     ToolbarComponent,
