7c39de852ae6a6eb6d6f4701982b0335fbe7b684
adrian_cerdeira <acwebdesign01@gmail.com>
2020-12-03 08:41:07 +0100

    Use viewchild to close luxbar menu

--- src/app/shared/toolbar/toolbar.component.ts
+++ src/app/shared/toolbar/toolbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-toolbar',
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./toolbar.component.scss']
 })
 export class ToolbarComponent implements OnInit {
+  @ViewChild('luxbarCheckbox')
+  checkbox!: ElementRef;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  closeMenu(): void {
+    this.checkbox.nativeElement.checked = false;
   }
 
 }
