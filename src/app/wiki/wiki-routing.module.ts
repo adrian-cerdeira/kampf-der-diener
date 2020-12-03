31df48a00a53a04e576a2cdb646bb235a9997616
adrian_cerdeira <acwebdesign01@gmail.com>
2020-12-03 11:24:28 +0100

    Detail Seite link erstellt

--- src/app/wiki/wiki-routing.module.ts
+++ src/app/wiki/wiki-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { WikiComponent } from './wiki/wiki.component';
+import { CardDetailsComponent } from './card-details/card-details.component';
 
 const routes: Routes = [
   {
     path: '',
     component: WikiComponent
+  },
+  {
+    path: ':id',
+    component: CardDetailsComponent
   },
 ];
 
--- src/app/wiki/wiki.module.ts
+++ src/app/wiki/wiki.module.ts
@@ -6,13 +6,15 @@ import { WikiRoutingModule } from './wiki-routing.module';
 import { WikiComponent } from './wiki/wiki.component';
 import { WikiListComponent } from './wiki-list/wiki-list.component';
 import { WikiListItemComponent } from './wiki-list-item/wiki-list-item.component';
+import { CardDetailsComponent } from './card-details/card-details.component';
 
 
 @NgModule({
   declarations: [
     WikiComponent,
     WikiListComponent,
-    WikiListItemComponent
+    WikiListItemComponent,
+    CardDetailsComponent
   ],
   imports: [
     CommonModule,
