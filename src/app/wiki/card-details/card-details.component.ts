a41d846afef54631be6fd46037fc921f766f6fcb
adrian_cerdeira <acwebdesign01@gmail.com>
2020-12-03 12:53:03 +0100

    test code

--- src/app/wiki/card-details/card-details.component.ts
+++ src/app/wiki/card-details/card-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { CardsService } from '../../cards/cards.service';
 
 @Component({
   selector: 'app-card-details',
@@ -6,10 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-details.component.scss']
 })
 export class CardDetailsComponent implements OnInit {
+  card: any;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private cardService: CardsService,
+  ) { }
 
   ngOnInit(): void {
+    const id = Number(this.route.snapshot.params.id);
+    this.card = this.cardService.getById(id);
   }
 
 }
