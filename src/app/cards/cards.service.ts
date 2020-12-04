7ba425a14950383ec5175051e75987a11529e04c
Adrian Cerdeira <acwebdesign01@gmail.com>
2020-12-04 11:10:07 +0100

    Service refactored

--- src/app/cards/cards.service.ts
+++ src/app/cards/cards.service.ts
@@ -6,11 +6,28 @@ import cards from './cards.json';
   providedIn: 'root',
 })
 export class CardsService {
-  private cards;
+  private cards: any;
 
   constructor() {
-    // Initialisierung des Karten Array
-    this.cards = this.createAll();
+    // Initialiserung des Karten Array mit alphabetischer Sortierung
+    this.cards = cards
+      .sort((a, b) => a.name.localeCompare(b.name))
+      .map((c, i) => {
+
+        return {
+          id: i + 1,
+          name: c.name,
+          img: c.img,
+          description: c.description,
+          effects: c.effects,
+          attack: c.attack,
+          life: c.life,
+          shield: c.shield,
+          crystal: c.crystal,
+          type: c.type,
+        };
+
+      });
   }
 
   getAll(): void {
@@ -38,48 +55,6 @@ export class CardsService {
 
   getByName(name: string): any {
     return this.cards.find((c: { name: string; }) => c.name === name);
-  }
-
-  private createAll(): any {
-    // Erstellt Karten Array in alphabetisch nach Namen sortiert
-    return cards
-      .sort((a, b) => a.name.localeCompare(b.name))
-      .map((c, i) => {
-
-        return this.create({
-          id: i + 1,
-          name: c.name,
-          img: c.img,
-          description: c.description,
-          effects: c.effects,
-          attack: c.attack,
-          life: c.life,
-          shield: c.shield,
-          crystal: c.crystal,
-          type: c.type,
-        });
-
-      });
-  }
-
-  private create(template: any): any {
-    const newCard = {
-      id: null,
-      name: null,
-      img: null,
-      description: null,
-      effects: null,
-      attack: null,
-      life: null,
-      shield: null,
-      crystal: null,
-      type: null,
-    };
-
-    return {
-      ...newCard,
-      ...template
-    };
   }
 
 }
