515c4e7bd6d8f67820d124bc707d5b32a5c9ee63
adrian_cerdeira <acwebdesign01@gmail.com>
2020-12-26 15:09:40 +0100

    added new changes

---
+++ src/app/wiki/wiki-filter/wiki-filter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wiki-filter',
+  templateUrl: './wiki-filter.component.html',
+  styleUrls: ['./wiki-filter.component.scss']
+})
+export class WikiFilterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
