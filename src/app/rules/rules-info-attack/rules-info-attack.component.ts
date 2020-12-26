f65cdce77ebe68d426a615747cf9d0628e36d489
Adrian Cerdeira <acwebdesign01@gmail.com>
2020-12-26 18:24:44 +0100

    Rules Page redesignt (#30)

---
+++ src/app/rules/rules-info-attack/rules-info-attack.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rules-info-attack',
+  templateUrl: './rules-info-attack.component.html',
+  styleUrls: ['./rules-info-attack.component.scss']
+})
+export class RulesInfoAttackComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/rules/rules.module.ts
+++ src/app/rules/rules.module.ts
@@ -6,8 +6,14 @@ import { RulesRoutingModule } from './rules-routing.module';
 
 import { RulesComponent } from './rules/rules.component';
 import { RulesInfoComponent } from './rules-info/rules-info.component';
+import { RulesInfoGameplayComponent } from './rules-info-gameplay/rules-info-gameplay.component';
+import { RulesInfoDefaultComponent } from './rules-info-default/rules-info-default.component';
+import { RulesInfoGamemodesComponent } from './rules-info-gamemodes/rules-info-gamemodes.component';
+import { RulesInfoGameComponent } from './rules-info-game/rules-info-game.component';
+import { RulesInfoGeneralComponent } from './rules-info-general/rules-info-general.component';
+import { RulesInfoAttackComponent } from './rules-info-attack/rules-info-attack.component';
+import { RulesInfoPartnerComponent } from './rules-info-partner/rules-info-partner.component';
 import { RulesMenuComponent } from './rules-menu/rules-menu.component';
-
 
 @NgModule({
   imports: [
@@ -18,6 +24,13 @@ import { RulesMenuComponent } from './rules-menu/rules-menu.component';
   declarations: [
     RulesComponent,
     RulesInfoComponent,
+    RulesInfoGameplayComponent,
+    RulesInfoDefaultComponent,
+    RulesInfoGamemodesComponent,
+    RulesInfoGameComponent,
+    RulesInfoGeneralComponent,
+    RulesInfoAttackComponent,
+    RulesInfoPartnerComponent,
     RulesMenuComponent,
   ],
 })
