9a5e9da76bc99d003402074c931546533fb08277
TDMario <mario_forrer@sluz.ch>
2021-01-03 19:22:07 +0100

    +Methods for damage calculation

--- src/app/game/move.ts
+++ src/app/game/move.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Card } from './card';
+import { Player } from './player';
 
 @Injectable({
   providedIn: 'root',
@@ -9,7 +11,15 @@ export class Move {
   constructor(
   ) { }
 
-  whoWins(playerACard: any, playerBCard: any): any {
-    return [playerACard, playerBCard];
+  battleBetweenCards(attackingCard: Card, defendingCard: Card): any {
+    defendingCard.setHitpoints(defendingCard.getHitpoints() - (attackingCard.getAttack() - defendingCard.getShield()));
+    attackingCard.setHitpoints(attackingCard.getHitpoints() - (defendingCard.getAttack() - attackingCard.getShield()));
+    return [attackingCard, defendingCard];
   }
+
+  attackOnPlayer(attackingCard: Card, player: Player): Player {
+    player.takeDamage(attackingCard.getAttack());
+    return player;
+  }
+
 }
