8b1c44dab82d2b786d6370eb7f99dab245df57a4
Adrian Cerdeira <acwebdesign01@gmail.com>
2021-01-03 20:03:57 +0100

    Card Location Enum separates File

---
+++ src/app/cards/card-location.ts
@@ -0,0 +1,13 @@
+export enum CardLocation {
+    undefined = 0,
+    inDeckPlayerA = 1,
+    inDeckPlayerB = 2,
+    inHandPlayerA = 3,
+    inHandPLayerB = 4,
+    DienerPlayerA = 5,
+    DienerPlayerB = 6,
+    ZauberPlayerA = 7,
+    ZauberPlayerB = 8,
+    FriedhofPlayerA = 9,
+    FriedhofPlayerB = 10,
+}
--- src/app/cards/cards.service.ts
+++ src/app/cards/cards.service.ts
@@ -58,17 +58,3 @@ export class CardsService {
   }
 
 }
-
-export enum CardLocation {
-  undefined = 0,
-  inDeckPlayerA = 1,
-  inDeckPlayerB = 2,
-  inHandPlayerA = 3,
-  inHandPLayerB = 4,
-  DienerPlayerA = 5,
-  DienerPlayerB = 6,
-  ZauberPlayerA = 7,
-  ZauberPlayerB = 8,
-  FriedhofPlayerA = 9,
-  FriedhofPlayerB = 10,
-}
