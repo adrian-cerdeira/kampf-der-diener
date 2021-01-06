d0a5e51beb9652c2f2d93161d0e335365a262677
Adrian Cerdeira <acwebdesign01@gmail.com>
2021-01-06 20:14:55 +0100

    Improve dialog texts

--- src/app/tutorial/tutorial/tutorial.component.ts
+++ src/app/tutorial/tutorial/tutorial.component.ts
@@ -155,10 +155,23 @@ export class TutorialComponent implements OnInit {
           title: 'Ende',
           content: `
                 <h1 class="title">Gewonnen</h1>
-                <p class="has-text-left">
+                <p>
                   Du hast dein Gegner besiegt!
                   <br />
                   Das Tutorial hast du erfolgreich abgeschlossen.
+                </p>
+                <br />
+                <h2 class="title">Probleme</h2>
+                <p>
+                  Falls technische Schwierigkeiten passiert sind, erstellen Sie bitte ein Ticket auf 
+                  <a href="https://github.com/adrian-cerdeira/kampf-der-diener/issues/new">
+                  <i class="fab fa-github"></i>
+                  Github - Kampf der Diener Issues
+                  </a>
+                  .
+                  <br />
+                  <br />
+                  Danke fürs Feedback!
                 </p>
                 `,
           index: 5,
@@ -177,7 +190,10 @@ export class TutorialComponent implements OnInit {
         In diesem Tutorial werden Sie das Spiel kennenlernen.
         <br />
         Schritt für Schritt werden Sie an das Spiel spielerisch herangeführt.
-        <br />
+      </p>
+      <br />
+      <h2 class="title">Probleme</h2>
+      <p>
         Falls technische Schwierigkeiten passiert sind, erstellen Sie bitte ein Ticket auf 
         <a href="https://github.com/adrian-cerdeira/kampf-der-diener/issues/new">
         <i class="fab fa-github"></i>
@@ -186,7 +202,7 @@ export class TutorialComponent implements OnInit {
         .
         <br />
         <br />
-        Danke fürs Feedback und viel Spass.
+        Danke fürs Feedback!
       </p>
       `,
       index: 1,
