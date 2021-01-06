9a5b7334bb55387e0c69c3d41468cf2726e27ef1
Adrian Cerdeira <acwebdesign01@gmail.com>
2021-01-06 21:30:22 +0100

    Erster Zug von Bot dargestellt

--- src/app/tutorial/tutorial/tutorial.component.ts
+++ src/app/tutorial/tutorial/tutorial.component.ts
@@ -32,17 +32,6 @@ export class TutorialComponent implements OnInit {
       this.start();
     }
 
-    // Turn 1 Bot
-    // this.game.drawCard(this.game.getPlayerB(), 13);
-
-    // Search for card to place
-    // let cardPos = this.game.searchCard(this.game.getPlayerB().getHandCards(), 5);
-
-    // Place card
-    // this.game.placeCard(this.game.getPlayerB(), cardPos); 
-
-
-
     // Turn 1 Player
     // this.game.drawCard(this.game.getPlayerA(), 59);
 
@@ -126,6 +115,11 @@ export class TutorialComponent implements OnInit {
             index: 2,
             isActive: true,
           });
+        } else {
+          // Turn 1 Bot
+          this.game.drawCard(this.playerBot, 13);
+          const newCardPosition = this.game.searchCard(this.playerBot.getHandCards(), 5);
+          this.game.placeDiener(this.playerBot, newCardPosition);
         }
         break;
       case 3:
