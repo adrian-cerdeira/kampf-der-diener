c05827861495c7fb4df1f33327724b0a0d86c3fa
Adrian Cerdeira <acwebdesign01@gmail.com>
2021-01-07 21:05:32 +0100

    Format shop and add update service

--- src/app/game/game/game.component.ts
+++ src/app/game/game/game.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UpdateService } from '../../shared/update.service';
+
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
@@ -7,7 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private updateService: UpdateService,
+  ) {
+    this.updateService.checkUpdate();
+  }
 
   ngOnInit(): void {
   }
--- src/app/shop/shop.module.ts
+++ src/app/shop/shop.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SharedModule } from '../shared/shared.module';
+
 import { ShopRoutingModule } from './shop-routing.module';
 
 import { ShopComponent } from './shop/shop.component';
@@ -9,8 +10,8 @@ import { ShopComponent } from './shop/shop.component';
 @NgModule({
   imports: [
     CommonModule,
-    ShopRoutingModule,
     SharedModule,
+    ShopRoutingModule,
   ],
   declarations: [
     ShopComponent
